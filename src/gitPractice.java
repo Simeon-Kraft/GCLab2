046ca10350260af0f6aa985ea9bc1e00b0342a14
Simeon Kraft <kraft.simeon.sk@gmail.com>
2018-10-09 11:31:54 -0400

    added practice reminder

---
+++ src/gitPractice.java
@@ -0,0 +1,18 @@
+
+public class gitPractice {
+
+	public static void main(String[] args) {
+		
+		int i= 0;
+		
+		while(i >= 0) {
+			
+			System.out.println("You should practice!");
+			
+			++i;
+		}
+
+	}
+
+
+}
